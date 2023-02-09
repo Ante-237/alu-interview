f11116e6dfbf6ae6688d3d1bda4b33f9979f381a
Ante-237 <akumawahanye@gmail.com>
2023-02-09 23:10:33 +0200

    fix pep8 errors

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -6,39 +6,39 @@
 
 # while characters < n:
 # if (n - characters)%2 == 0:
-# perform 
+# perform
 # copy = characters
 #           characters += copy
 #          operations += 2
 #      else:
-#if operations == 0:
+# if operations == 0:
 #                operations += 1
 #  characters += copy
 #  operations += 1
 # return operations
-# Minimum number of operations 
+# Minimum number of operations
 """
     perform minimum
     operations
-    """
+  """
+
 
 def minOperations(n):
     """
-    get minimum double , and one 
+    get minimum double , and one
     operations
     """
     operations = 0
     characters = 2
     while n > 1:
-        # ensure characters count is greater than 0 
+        # ensure characters count is greater than 0
         # else return a zero
         while n % characters == 0:
             # while difference is even increase
-            # operations by character count 
+            # operations by character count
             operations += characters
             # reduce the characters count by half
             n = n / characters
-        # increase the characters 
+        # increase the characters
         characters += 1
     return operations
-
